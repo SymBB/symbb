5b508c16ea23c9013a05ff25c6ab0b464df83126
Christian Wielath <christian.wielath@tyre24.de>
2014-01-17 12:38:20 +0100

    adding prefix to acl tables

--- Core/SystemBundle/DependencyInjection/SymBBCoreSystemExtension.php
+++ Core/SystemBundle/DependencyInjection/SymBBCoreSystemExtension.php
@@ -24,6 +24,25 @@ class SymBBCoreSystemExtension extends Extension implements PrependExtensionInte
         $loader->load('services.yml');
         $loader->load('doctrine.yml');
 
+        $prefix = '';
+        foreach ($container->getExtensions() as $name => $extension) {
+            // web profiler is only activated in the dev env.
+            if('web_profiler' == $name){
+                $prefix  = 'dev_';
+                break;
+            }
+        }
+        
+        foreach ($container->getExtensions() as $name => $extension) {
+            if($name == 'security'){                
+                $config['acl']['tables']['class']                       = $prefix.'acl_classes';
+                $config['acl']['tables']['entry']                       = $prefix.'acl_entries';
+                $config['acl']['tables']['object_identity']             = $prefix.'acl_object_identities';
+                $config['acl']['tables']['object_identity_ancestors']   = $prefix.'acl_object_identity_ancestors';
+                $config['acl']['tables']['security_identity']           = $prefix.'acl_security_identities';
+                $container->prependExtensionConfig($name, $config);
+            }
+        }
     }
         
     public function load(array $configs, ContainerBuilder $container)
