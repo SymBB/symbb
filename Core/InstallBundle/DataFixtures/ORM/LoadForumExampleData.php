505de2da3247bfcacc973f35d654b3d6f97bb83b
Christian Wielath <christian.wielath@tyre24.de>
2014-02-04 13:32:55 +0100

    remove forum example data from data fixtures

--- Core/InstallBundle/DataFixtures/ORM/LoadForumExampleData.php
+++ Core/InstallBundle/DataFixtures/ORM/LoadForumExampleData.php
@@ -35,6 +35,7 @@ class LoadForumExampleData extends AbstractFixture implements OrderedFixtureInte
     public function load(ObjectManager $manager)
     {
         
+        /*
         $forum = new \SymBB\Core\ForumBundle\Entity\Forum();
         $forum->setActive(1);
         $forum->setName('Main Category');
@@ -254,7 +255,7 @@ class LoadForumExampleData extends AbstractFixture implements OrderedFixtureInte
             'SYMBB_FORUM_RATING#VIEW',
             'SYMBB_FORUM_RATING#EDIT'
         ), $forum6, $this->getReference('admin-group'));
-        
+        */
     }
 
     /**
