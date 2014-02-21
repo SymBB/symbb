6d68f9171307cec4cd9ff4f3df38840fb49b1b8d
Christian Wielath <christian.wielath@tyre24.de>
2014-02-21 10:16:21 +0100

    add posibility to remove the own raiting

--- Extension/RatingBundle/Controller/DefaultController.php
+++ Extension/RatingBundle/Controller/DefaultController.php
@@ -1,29 +1,29 @@
-<?
+<?php
 /**
-*
-* @package symBB
-* @copyright (c) 2013-2014 Christian Wielath
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-*
-*/
+ *
+ * @package symBB
+ * @copyright (c) 2013-2014 Christian Wielath
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
 
 namespace SymBB\Extension\RatingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-
-class DefaultController  extends Controller 
+class DefaultController extends Controller
 {
-    
-    public function ratePostAction($name, $id, $like){
-        
+
+    public function ratePostAction($name, $id, $like)
+    {
+
         $post = $this->get('doctrine')->getRepository('SymBBCoreForumBundle:Post', 'symbb')
             ->find($id);
-        
-        $user   = $this->getUser();
-        
-        if(is_object($user) && $user->getId() > 0){
-            if($like === 'like'){
+
+        $user = $this->getUser();
+
+        if (is_object($user) && $user->getId() > 0) {
+            if ($like === 'like') {
                 $this->addPostLike($post, $user);
             } else {
                 $this->addPostLike($post, $user, true);
@@ -31,85 +31,84 @@ class DefaultController  extends Controller
         }
 
         $response = $this->forward('SymBBCoreForumBundle:FrontendTopic:show', array(
-            'name'  => '',
+            'name' => '',
             'id' => $post->getTopic()->getId(),
         ));
-        
+
         return $response;
     }
-    
+
     protected function addPostLike(
-            \SymBB\Core\ForumBundle\Entity\Post $post, 
-            \SymBB\Core\UserBundle\Entity\UserInterface $user, 
-            $asDislike = false
-    ){
-        
-        $likes      = $this->get('doctrine')->getRepository('SymBBExtensionRatingBundle:Like', 'symbb')
+    \SymBB\Core\ForumBundle\Entity\Post $post, \SymBB\Core\UserBundle\Entity\UserInterface $user, $asDislike = false
+    )
+    {
+
+        $likes = $this->get('doctrine')->getRepository('SymBBExtensionRatingBundle:Like', 'symbb')
             ->findBy(array('post' => $post, 'user' => $user));
-        
-        $dislikes   = $this->get('doctrine')->getRepository('SymBBExtensionRatingBundle:Dislike', 'symbb')
+
+        $dislikes = $this->get('doctrine')->getRepository('SymBBExtensionRatingBundle:Dislike', 'symbb')
             ->findBy(array('post' => $post, 'user' => $user));
-        
-        $myLikes    = array();
+
+        $myLikes = array();
         $myDislikes = array();
-        
-        $em         = $this->get('doctrine')->getManager('symbb');
-        
-        foreach($likes as $like){
-            if($like->getUser()->getId() === $user->getId()){
+
+        $em = $this->get('doctrine')->getManager('symbb');
+
+        foreach ($likes as $like) {
+            if ($like->getUser()->getId() === $user->getId()) {
                 $myLikes[] = $like;
             }
         }
-        
-        foreach($dislikes as $dislike){
-            if($dislike->getUser()->getId() === $user->getId()){
+
+        foreach ($dislikes as $dislike) {
+            if ($dislike->getUser()->getId() === $user->getId()) {
                 $myDislikes[] = $dislike;
             }
         }
-        
+
         // if the user "like" it
-        if(!$asDislike){
-            
+        if (!$asDislike) {
+
             // remove "dislikes"
-            foreach($myDislikes as $myDislike){
+            foreach ($myDislikes as $myDislike) {
                 //$post->removeDislike($myDislike);
                 $em->remove($myDislike);
             }
-            
+
             // create a new "like" if no one exist
-            if(empty($myLikes)){
+            if (empty($myLikes)) {
                 $myLike = new \SymBB\Extension\RatingBundle\Entity\Like();
                 $myLike->setUser($user);
                 $myLike->setPost($post);
-            }
-            
-            // if we create a new "like" then add it
-            if(isset($myLike)){
                 $em->persist($myLike);
+                // i again then delete
+            } else {
+                foreach ($myLikes as $myLike) {
+                    $em->remove($myLike);
+                }
             }
-           
+
         } else {
             // remove "likes"
-            foreach($myLikes as $myLike){
+            foreach ($myLikes as $myLike) {
                 //$post->removeLike($myLike);
                 $em->remove($myLike);
             }
-            
+
             // create a new "dislike" if no one exist
-            if(empty($myDislikes)){
+            if (empty($myDislikes)) {
                 $myDislike = new \SymBB\Extension\RatingBundle\Entity\Dislike();
                 $myDislike->setUser($user);
                 $myDislike->setPost($post);
-            }
-            
-            // if we create a new "dislike" then add it
-            if(isset($myDislike)){
                 $em->persist($myDislike);
+                // i again then delete
+            } else {
+                foreach ($myDislikes as $myDislike) {
+                    $em->remove($myDislike);
+                }
             }
         }
-        
+
         $em->flush();
-    
     }
-    
 }
