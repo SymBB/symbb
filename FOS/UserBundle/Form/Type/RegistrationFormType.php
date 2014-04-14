cd18d8a465b3fb11f25b1d5bb954867c043486e5
Christian Wielath <christian.wielath@tyre24.de>
2014-04-14 13:26:36 +0200

    fixed ##17 closed #17

--- FOS/UserBundle/Form/Type/RegistrationFormType.php
+++ FOS/UserBundle/Form/Type/RegistrationFormType.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of the FOSUserBundle package.
  *
@@ -15,26 +14,37 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistrationFormType extends \FOS\UserBundle\Form\Type\RegistrationFormType
 {
-
+    
+    /**
+     *
+     * @var \SymBB\Core\UserBundle\DependencyInjection\UserManager
+     */
+    protected $usermanager;
+    
+    public function __construct($class, $usermanager)
+    {
+        parent::__construct($class);
+        $this->usermanager = $usermanager;
+    }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', 'email', array('label' => 'form.email', 'translation_domain' => 'symbb_frontend', 'attr' => array('placeholder' => 'Your E-Mail')))
+            ->add('email', 'email', array('label' => 'form.email', 'translation_domain' => 'symbb_frontend', 'attr' => array('placeholder' => 'Your E-Mail'),
+                ))
             ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'symbb_frontend', 'attr' => array('placeholder' => 'Your Username')))
             ->add('plainPassword', 'repeated', array(
-                'type' => 'password',
-                'options' => array('translation_domain' => 'symbb_frontend'),
-                'first_options' => array('label' => 'form.password', 'attr' => array('placeholder' => 'Your Password')),
-                'second_options' => array('label' => 'form.password_confirmation', 'attr' => array('placeholder' => 'Retype your Password')),
-                'invalid_message' => 'fos_user.password.mismatch',
-            ))
-            ->add('terms', 'checkbox', array('label' => 'Read and accepted Terms of Use', 'translation_domain' => 'symbb_frontend', 'mapped' => false, 'required' => true))
-            
-        ;
+                    'type' => 'password',
+                    'options' => array('translation_domain' => 'symbb_frontend'),
+                    'first_options' => array('label' => 'form.password', 'attr' => array('placeholder' => 'Your Password')),
+                    'second_options' => array('label' => 'form.password_confirmation', 'attr' => array('placeholder' => 'Retype your Password')),
+                    'invalid_message' => 'fos_user.password.mismatch',
+                    'constraints' => $this->usermanager->getPasswordValidatorConstraints()
+                )   
+            )
+            ->add('terms', 'checkbox', array('label' => 'Read and accepted Terms of Use', 'translation_domain' => 'symbb_frontend', 'mapped' => false, 'required' => true));
     }
 
-    
     public function getName()
     {
         return 'symbb_fos_user_registration';
