3d63dcadf6304d25d4f6f9490b64b492bf585941
=Christian Wielath <info@script-brain.de>
2014-04-15 22:48:48 +0200

    adding cache controll to angular template files

--- Template/DefaultBundle/Controller/AngularController.php
+++ Template/DefaultBundle/Controller/AngularController.php
@@ -16,9 +16,16 @@ class AngularController extends Controller
 
     public function templateFileAction($file)
     {
-        return $this->render(
+        $response = $this->render(
             'SymBBTemplateDefaultBundle:Angular:'.$file.'.html.twig',
             array()
         );
+        
+        // all angular templates should be public, they dont contains some private informations. 
+        // all variables are parsed at the frontend
+        $response->setPublic();
+        $response->setSharedMaxAge(600);
+        
+        return $response;
     }
 }
