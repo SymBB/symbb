ea533b123c18e2a638cdd6741f3dab4c4a1602f6
Christian Wielath <christian.wielath@tyre24.de>
2014-04-29 13:03:04 +0200

    fix focus + selection after using bbcode button

--- Core/BBCodeBundle/Resources/public/js/bbeditor.js
+++ Core/BBCodeBundle/Resources/public/js/bbeditor.js
@@ -11,7 +11,10 @@ function createBBEditor(parentDiv){
             } else if (element.selectionStart || element.selectionStart == '0') {
                 var startPos = element.selectionStart;
                 var endPos = element.selectionEnd;
-                element.value = element.value.substring(0, startPos) + tagCode.replace('{0}', element.value.substring(startPos, endPos)) + element.value.substring(endPos, element.value.length);
+                var insert = tagCode.replace('{0}', element.value.substring(startPos, endPos));
+                element.value = element.value.substring(0, startPos) + insert + element.value.substring(endPos, element.value.length);
+                element.focus();
+                element.selectionStart = endPos + insert.length;
             } else {
                 element.focus();
                 element.value += tagCode.replace('{0}', '');
