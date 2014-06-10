ee6a81edea4fe8ce59e510537375b77c7e4f1586
Christian Wielath <info@script-brain.de>
2014-06-10 19:13:01 +0200

    changing style

--- Template/DefaultBundle/Resources/public/js/angular/module.js
+++ Template/DefaultBundle/Resources/public/js/angular/module.js
@@ -5,6 +5,19 @@ angular.module('symbbTemplateModule', []).
             response: function(response) {
                 $timeout(function(){
                     $('.symbb_post_embeded_image_link').magnificPopup({type:'image'});
+                    $('.symbb_post_block').each(function(key, element){
+                        $(element).find('.signature').each(function(key2, signature){
+                            $(element).find('.infos').css('padding-bottom', $(signature).innerHeight() );
+                        });
+                        var left = $(element).find('.userinfo');
+                        var right = $(element).find('.infos');
+                        if($(left).innerHeight() > $(right).innerHeight()){
+                            $(right).height($(left).innerHeight());
+                        } else if($(left).innerHeight() < $(right).innerHeight()){
+                            $(left).height($(right).innerHeight());
+                        }
+
+                    });
                 }, 1);
                 // Return the response or promise.
                 return response || $q.when(response);
