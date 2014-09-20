43bcaba2c3f2cccc794536c638c1c050b37ebc04
Christian Wielath <info@script-brain.de>
2014-09-20 23:51:56 +0200

    remove js from core bundles and move it to template bundle

--- Template/DefaultBundle/Resources/public/js/frontend/3_directives.js
+++ Template/DefaultBundle/Resources/public/js/frontend/3_directives.js
@@ -1,3 +1,26 @@
+symbbControllers.directive('symbbTopicMove', ['$http', '$timeout', function($http, $timeout) {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: angularConfig.getSymfonyTemplateRoute('forum_topic_move'),
+        link: function(scope, elm, attrs) {
+            scope.id = attrs.paramId;
+            $(elm).click(function(){
+                var dialog = $(this).find('.modal')
+                $(dialog).find('.btn-move').click(function(){
+                    var select = $(dialog).find('select');
+                    var forumId = $(select).val();
+                    var topicId = attrs.paramId;
+                    $http.post(angularConfig.getSymfonyRoute('symbb_api_forum_topic_move', {forum: forumId, id:topicId})).success(function(data) {
+
+                    });
+                    $(dialog).modal('hide');
+                });
+            })
+        }
+    };
+}]);
+
 symbbControllers.directive('symbbUserAutocomplete', ['$http', '$timeout', function($http, $timeout) {
     return {
         restrict: 'E',
@@ -49,4 +72,3 @@ symbbControllers.directive('symbbUserAutocomplete', ['$http', '$timeout', functi
         }
     };
 }]);
-
