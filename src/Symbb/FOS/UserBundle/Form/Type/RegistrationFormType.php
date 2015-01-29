ce9aefa0000495afc646c74c0b868b7abc0fd29c
Christian Wielath <info@script-brain.de>
2015-01-29 21:53:49 +0100

    adding recaptcha for registration

--- src/Symbb/FOS/UserBundle/Form/Type/RegistrationFormType.php
+++ src/Symbb/FOS/UserBundle/Form/Type/RegistrationFormType.php
@@ -42,8 +42,14 @@ class RegistrationFormType extends \FOS\UserBundle\Form\Type\RegistrationFormTyp
                     'constraints' => $this->usermanager->getPasswordValidatorConstraints()
                 )
             )
-            ->add('recaptcha', 'formextra_recaptcha')
             ->add('terms', 'checkbox', array('label' => 'Read and accepted Terms of Use', 'translation_domain' => 'symbb_frontend', 'mapped' => false, 'required' => true));
+
+        $builder->add('recaptcha', 'formextra_recaptcha', array(
+            'widget_options' => array(
+                'theme' => 'white', // blackglass, clean, red is the predefined themes.
+            ),
+            'mapped' => false
+        ));
     }
 
     public function getName()
