ec3f25fd92ed5b44a4dc3ff6aec640d8cca8e5d4
Christian Wielath <christian.wielath@tyre24.de>
2015-02-19 14:36:24 +0100

    fix image upload in editor

--- src/Symbb/Core/ForumBundle/Security/Authorization/ForumVoter.php
+++ src/Symbb/Core/ForumBundle/Security/Authorization/ForumVoter.php
@@ -29,6 +29,10 @@ class ForumVoter extends AbstractVoter implements VoterInterface
     const SPLIT_TOPIC = 'split_topic';
     const UPLOAD_FILE = 'upload_file';
 
+    /**
+     * this will define the groups for the acl form later
+     * @return array
+     */
     public function getGroupedAttributes()
     {
         return array(
@@ -50,34 +54,34 @@ class ForumVoter extends AbstractVoter implements VoterInterface
         );
     }
 
+    /**
+     * this are the list of default access lists
+     * @return array
+     */
     public function getAccessSets()
     {
-        return array(
-            // no access
-            "default_1" => array(),
-            // readonly
-            "default_2" => array(
-                self::VIEW
-            ),
-            // normal
-            "default_3" => array(
-                self::VIEW,
+
+        $default = array();
+
+        $guest = array(
+            self::VIEW
+        );
+
+        $user = array_merge($guest,
+            array(
                 self::CREATE_POST,
                 self::CREATE_TOPIC
-            ),
-            // full (add extension access)
-            "default_4" => array(
-                self::VIEW,
-                self::CREATE_POST,
-                self::CREATE_TOPIC,
+            )
+        );
+        
+        $userFull = array_merge($user,
+            array(
                 self::UPLOAD_FILE
-            ),
-            // moderator
-            "default_5" => array(
-                self::VIEW,
-                self::CREATE_POST,
-                self::CREATE_TOPIC,
-                self::UPLOAD_FILE,
+            )
+        );
+
+        $mod = array_merge($userFull,
+            array(
                 self::EDIT_POST,
                 self::EDIT_TOPIC,
                 self::DELETE_POST,
@@ -86,6 +90,19 @@ class ForumVoter extends AbstractVoter implements VoterInterface
                 self::MOVE_TOPIC,
                 self::SPLIT_TOPIC,
             )
+        );
+
+        return array(
+            // no access
+            "default_1" => $default,
+            // readonly
+            "default_2" => $guest,
+            // normal
+            "default_3" => $user,
+            // full (add extension access)
+            "default_4" => $userFull,
+            // moderator
+            "default_5" => $mod
         );
     }
 }
